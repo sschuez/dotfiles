7da800a7cc12e51b5629a984b69bc532f5b35819
Stephen <stephens@hey.com>
2024-06-09 20:43:01 +0200

    Fix themes script

--- scripts/apply_theme.sh
+++ scripts/apply_theme.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define available themes
-THEME_NAMES=("Tokyo Night" "Catppuccin Mocha" "Catppuccin Latte" "Nord" "Everforest" "Gruvbox" "Kanagawa" "Rose Pine" "Jellybeans")
+THEME_NAMES=("Tokyo Night" "Catppuccin Mocha" "Catppuccin Latte" "Solarized" "Nord" "Everforest" "Gruvbox" "Kanagawa" "Rose Pine" "Jellybeans")
 
 # Use gum to select a theme (assuming gum is installed)
 THEME=$(gum choose "${THEME_NAMES[@]}" --header "Choose your theme" --height 9 | tr '[:upper:]' '[:lower:]' | sed 's/ /-/g')
@@ -13,7 +13,7 @@ THEME=$(gum choose "${THEME_NAMES[@]}" --header "Choose your theme" --height 9 |
 THEME_DIR="$HOME/code/dotfiles/themes"
 NEOVIM_THEME_DIR="$THEME_DIR/neovim"
 TMUX_THEME_DIR="$THEME_DIR/tmux"
-CURRENT_THEME_CONF="$HOME/.tmux.conf"
+CURRENT_THEME_CONF="$HOME/code/dotfiles/tmux/.tmux.conf"
 TMUX_THEME_CONF="$TMUX_THEME_DIR/${THEME}.conf"
 
 # Function to apply the theme to iTerm2 using Python script
@@ -27,16 +27,9 @@ apply_iterm_theme() {
 # Apply theme to tmux
 apply_tmux_theme() {
 	if [ -f "$TMUX_THEME_CONF" ]; then
-		# Backup the current tmux config
-		cp "$CURRENT_THEME_CONF" "${CURRENT_THEME_CONF}.bak"
-
-		# Clear any existing theme settings from the tmux config
-		sed -i '/# --> BEGIN THEME/,/# --> END THEME/d' "$CURRENT_THEME_CONF"
-
-		# Append the new theme configuration
-		echo "# --> BEGIN THEME" >>"$CURRENT_THEME_CONF"
-		cat "$TMUX_THEME_CONF" >>"$CURRENT_THEME_CONF"
-		echo "# --> END THEME" >>"$CURRENT_THEME_CONF"
+		# Replace the last line in the tmux config with the new theme's source file reference
+		sed -i '' '$ d' "$CURRENT_THEME_CONF"
+		echo "source-file \"$TMUX_THEME_CONF\"" >>"$CURRENT_THEME_CONF"
 
 		# Reload tmux configuration
 		tmux source-file "$CURRENT_THEME_CONF"
@@ -52,7 +45,7 @@ apply_neovim_theme() {
 	local neovim_theme="$NEOVIM_THEME_DIR/$THEME.lua"
 	if [ -f "$neovim_theme" ]; then
 		cp "$neovim_theme" "$HOME/.config/nvim/lua/plugins/theme.lua"
-		echo "Applied Neovim theme."
+		echo "Applied Neovim theme: $THEME"
 	else
 		echo "Neovim theme file not found."
 	fi
@@ -63,8 +56,6 @@ apply_iterm_theme
 apply_tmux_theme
 apply_neovim_theme
 
-# Restart iTerm2 to apply the new theme
-osascript -e 'tell application "iTerm" to reload preferences'
-
 echo "Theme applied: $THEME"
+tmux source-file "$CURRENT_THEME_CONF"
 echo "Restart your terminal for a full theme change."

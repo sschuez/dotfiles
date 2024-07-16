04289e2ea1428b334cc2a51a05abe83f958c8502
Stephen <stephens@hey.com>
2024-07-16 11:57:28 +0200

    Add gruvbox light to themes

--- scripts/apply_theme.sh
+++ scripts/apply_theme.sh
@@ -2,17 +2,18 @@
 
 # Define available themes
 THEME_NAMES=(
-	"Tokyo Night"
-	"Catppuccin Mocha"
-	"Catppuccin Latte"
-	"Nord"
-	"Gruvbox"
-	"Kanagawa"
-	"Rose Pine"
-	"Everforest Light"
-	"Everforest Dark"
-	"Jellybeans"
-	"Cancel"
+  "Tokyo Night"
+  "Catppuccin Mocha"
+  "Catppuccin Latte"
+  "Nord"
+  "Gruvbox"
+  "Gruvbox Light"
+  "Kanagawa"
+  "Rose Pine"
+  "Everforest Light"
+  "Everforest Dark"
+  "Jellybeans"
+  "Cancel"
 )
 
 # Use gum to select a theme (assuming gum is installed)
@@ -20,8 +21,8 @@ THEME=$(gum choose "${THEME_NAMES[@]}" --header "Choose your theme" --height 9 |
 
 # Exit if no theme is selected
 if [ -z "$THEME" ] || [ "$THEME" == "cancel" ]; then
-	echo "No theme selected."
-	exit 0
+  echo "No theme selected."
+  exit 0
 fi
 
 # Define theme directories
@@ -35,47 +36,47 @@ ALACRITTY_THEME_CONF="$ALACRITTY_THEME_DIR/${THEME}.toml"
 
 # Function to apply the theme to iTerm2 using Python script -> currently not needed
 apply_iterm_theme() {
-	export THEME=$THEME
-	source ~/venvs/iterm2-env/bin/activate
-	python3 $HOME/code/dotfiles/scripts/change_color_theme.py
-	deactivate
+  export THEME=$THEME
+  source ~/venvs/iterm2-env/bin/activate
+  python3 $HOME/code/dotfiles/scripts/change_color_theme.py
+  deactivate
 }
 
 # Apply theme to tmux
 apply_tmux_theme() {
-	if [ -f "$TMUX_THEME_CONF" ]; then
-		# Replace the last line in the tmux config with the new theme's source file reference
-		sed -i '' '$ d' "$CURRENT_THEME_CONF"
-		echo "source-file \"$TMUX_THEME_CONF\"" >>"$CURRENT_THEME_CONF"
+  if [ -f "$TMUX_THEME_CONF" ]; then
+    # Replace the last line in the tmux config with the new theme's source file reference
+    sed -i '' '$ d' "$CURRENT_THEME_CONF"
+    echo "source-file \"$TMUX_THEME_CONF\"" >>"$CURRENT_THEME_CONF"
 
-		# Reload tmux configuration
-		tmux source-file "$CURRENT_THEME_CONF"
+    # Reload tmux configuration
+    tmux source-file "$CURRENT_THEME_CONF"
 
-		echo "Applied tmux theme: $THEME"
-	else
-		echo "tmux theme configuration file for '$THEME' not found."
-	fi
+    echo "Applied tmux theme: $THEME"
+  else
+    echo "tmux theme configuration file for '$THEME' not found."
+  fi
 }
 
 # Apply theme to Alacritty
 apply_alacritty_theme() {
-	if [ -f "$ALACRITTY_THEME_CONF" ]; then
-		cp "$ALACRITTY_THEME_CONF" "$HOME/.config/alacritty/theme.toml"
-		echo "Applied Alacritty theme: $THEME"
-	else
-		echo "Alacritty theme configuration file for '$THEME' not found."
-	fi
+  if [ -f "$ALACRITTY_THEME_CONF" ]; then
+    cp "$ALACRITTY_THEME_CONF" "$HOME/.config/alacritty/theme.toml"
+    echo "Applied Alacritty theme: $THEME"
+  else
+    echo "Alacritty theme configuration file for '$THEME' not found."
+  fi
 }
 
 # Apply theme to Neovim
 apply_neovim_theme() {
-	local neovim_theme="$NEOVIM_THEME_DIR/$THEME.lua"
-	if [ -f "$neovim_theme" ]; then
-		cp "$neovim_theme" "$HOME/.config/nvim/lua/plugins/theme.lua"
-		echo "Applied Neovim theme: $THEME"
-	else
-		echo "Neovim theme file not found."
-	fi
+  local neovim_theme="$NEOVIM_THEME_DIR/$THEME.lua"
+  if [ -f "$neovim_theme" ]; then
+    cp "$neovim_theme" "$HOME/.config/nvim/lua/plugins/theme.lua"
+    echo "Applied Neovim theme: $THEME"
+  else
+    echo "Neovim theme file not found."
+  fi
 }
 
 # Apply the selected theme

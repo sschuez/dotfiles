722659f7e2479860bef7ae0dc8f430c841171699
Stephen Schuez <stephens@hey.com>
2025-08-21 10:35:08 +0200

    update scripts to work with apple workflow

--- scripts/tmux_work.sh
+++ scripts/tmux_work.sh
@@ -1,10 +1,10 @@
-#!/bin/zsh
+#!/bin/bash
 
 # Explicitly set the PATH
 export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/homebrew/bin:$PATH"
 
-# Source the zshrc file to load environment variables and functions
-source ~/.zshrc
+# Source the bash file to load environment variables and functions
+source ~/.bashrc
 
 # Open Alacritty and execute the ta command in the background
-nohup /Applications/Alacritty.app/Contents/MacOS/alacritty -e zsh -ic 'source ~/.zshrc; ta; exec zsh' > /dev/null 2>&1 &
+nohup /Applications/Alacritty.app/Contents/MacOS/alacritty -e bash -ic 'source ~/.bashrc; ta; exec bash' > /dev/null 2>&1 &

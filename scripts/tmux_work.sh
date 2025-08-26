48ecefaf2470b32f59f1c454c18a8f4cc3e02ff1
Stephen Schuez <stephens@hey.com>
2025-08-26 11:26:58 +0200

    futher leanify tmux_work

--- scripts/tmux_work.sh
+++ scripts/tmux_work.sh
@@ -4,7 +4,7 @@
 export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/homebrew/bin:$PATH"
 
 # Source the bash file to load environment variables and functions
-source ~/.bashrc
+# source ~/.bashrc
 
 # Open Alacritty and execute the ta command in the background
-nohup /Applications/Alacritty.app/Contents/MacOS/alacritty -e bash -ic 'source ~/.bashrc; ta; exec bash' > /dev/null 2>&1 &
+nohup /Applications/Alacritty.app/Contents/MacOS/alacritty -e bash -ic 'source ~/.bashrc; ta; exec bash' >/dev/null 2>&1 &
